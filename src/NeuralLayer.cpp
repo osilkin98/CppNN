3bb70c407b3d50f16acdf1bc24f86a9d3defaafe
Oleg Silkin <o.silkin98@gmail.com>
2018-06-14 18:36:43 -0500

    Moved all the source files to a new directory, labeled /src/, and created a new class to deal with the actual handling of the Matrices, as we don't want to create space in memory for a weight matrix & bias vector for the input vector
    Signed-off-by: Oleg Silkin <o.silkin98@gmail.com>

--- src/NeuralLayer.cpp
+++ src/NeuralLayer.cpp
@@ -17,14 +17,35 @@ void NeuralLayer::clean(void) {
 }
 
 NeuralLayer::NeuralLayer(const size_t layer_size, const size_t previous_layer_size = 0) {
+    data = new NeuralMatrix(layer_size, 1);
     if(previous_layer_size) { // if we have a specifed size for the previous layer
-        data = new NeuralMatrix(layer_size, 1);
         weights = new NeuralMatrix(layer_size, previous_layer_size);
         bias = new NeuralMatrix(layer_size, 1);
     } else { // otherwise this is the first (input) layer, so we don't create weights + bias
-        data = new NeuralMatrix(layer_size, 1);
         bias = NULL;
         weights = NULL;
+    }
+}
+
+NeuralLayer::NeuralLayer(const NeuralLayer &other) {
+    // N should be the same for each matrix
+    data = new NeuralMatrix(other.data -> N, 1);
+    // this is only in the case of the input matrix, which has no weight or bias matrix
+    if(!other.weights) {
+        return;
+    }
+    bias = new NeuralMatrix(other.bias -> N, 1); // for all the matrices
+    weights = new NeuralMatrix(other.weights -> N, other.weights -> M);
+
+    size_t N = other.data -> N, M = other.weights -> M;
+    register size_t i, j;
+
+    for(i = 0; i < N; ++i) {
+        data -> matrix[i][0] = other.data -> matrix[i][0];
+        bias -> matrix[i][0] = other.bias -> matrix[i][0];
+        for(j = 0; j < M; ++j) {
+            weights -> matrix[i][j] = other.weights -> matrix[i][j];
+        }
     }
 }
 
