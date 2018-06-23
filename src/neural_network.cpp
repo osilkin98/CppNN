174d2f0eef8c21bc928af66f5607b6dcf1386aa9
Oleg Silkin <o.silkin98@gmail.com>
2018-06-22 20:42:55 -0500

    implemented method to retrieve data after Neural Network's forward pass
    Signed-off-by: Oleg Silkin <o.silkin98@gmail.com>

--- src/neural_network.cpp
+++ src/neural_network.cpp
@@ -62,8 +62,15 @@ void NeuralNetwork::set_data(const long double *data, const size_t N) const {
     }
 }
 
+// creates a copy of the Neural Network's produced data
 std::vector<long double>& NeuralNetwork::get_data_vector() const {
-
+    size_t access = layers.size() - 1;
+    size_t N = layers[access] -> N;
+    std::vector<long double> data(N);
+    for(register size_t i = 0; i < N; ++i) {
+        data[i] = layers[access] -> data -> matrix[i][0] -> function;
+    }
+    return data;
 }
 
 
