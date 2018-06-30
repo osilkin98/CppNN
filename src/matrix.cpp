4e46fd4909fcbf6e5fa869a721789e2e960108b3
Oleg Silkin <o.silkin98@gmail.com>
2018-06-29 21:33:19 -0500

    Implemented operator methods from matrix.h
    Signed-off-by: Oleg Silkin <o.silkin98@gmail.com>

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -3,7 +3,8 @@
 //
 
 #include "matrix.h"
-
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "OCDFAInspection"
 
 // this operator will be used when creating error vectors through back-propogation
 Matrix<long double>::Matrix(const NeuralMatrix& other, bool using_data) :
@@ -35,6 +36,10 @@ template <>
 Matrix<long double>* Matrix<long double>::operator-(const NeuralMatrix *other) const {
     if(equal_size(*other)) {
         Matrix<long double> *difference = new Matrix<long double>(N, M);
+        if(difference == nullptr) {
+            std::cerr << "Allocation failed at " << this << "\n";
+            return nullptr;
+        }
         register size_t i, j;
         for(i = 0; i < N; ++i) {
             for(j = 0; j < M; ++j) {
@@ -52,6 +57,10 @@ template <>
 Matrix<long double> *Matrix<long double>::hadamard_product(const NeuralMatrix *other) const {
     if(equal_size(*other)) {
         Matrix<long double> *product = new Matrix<long double>(N, M);
+        if(product == nullptr) {
+            std::cerr << "Allocation failed at " << this << "\n";
+            return nullptr;
+        }
         register size_t i, j;
         for(i = 0; i < N; ++i) {
             for(j = 0; j < M; ++j) {
@@ -64,3 +73,54 @@ Matrix<long double> *Matrix<long double>::hadamard_product(const NeuralMatrix *o
         throw std::length_error("Dimensions mismatch");
     }
 }
+
+
+// there's likely a better way to do these multiplications than to write specific cases for the matrices
+// using something like iterators for data members and then just iterating through and returning a vector of
+// doubles which is the resuult of whatever multiplicatios
+template <>
+Matrix<long double>* Matrix<long double>::operator*(const NeuralMatrix* other) const {
+    if(M == other -> N) {
+        Matrix<long double> *product = new Matrix<long double>(N, other -> M);
+        if(product == nullptr) {
+            std::cerr << "Allocation failed at " << this << "\n";
+            return nullptr;
+        }
+        register size_t i, j, k;
+        for(i = 0; i < N; ++i) {
+            for(j = 0; j < other -> M; ++j) {
+                product -> matrix[i][j] = 0;
+                for(k = 0; k < M; ++k) {
+                    product -> matrix[i][j] += matrix[i][k] * other -> matrix[k][j] -> function;
+                }
+            }
+        }
+        return product;
+    } else {
+        throw std::length_error("Dimension mismatch");
+    }
+}
+
+
+template <>
+Matrix<long double>* Matrix<long double>::operator+(const NeuralMatrix* other) const {
+    if(equal_size(*other)) {
+        Matrix<long double> *difference = new Matrix<long double>(N, M);
+        if(difference == nullptr) {
+            std::cerr << "Allocation failed at " << this << "\n";
+            return nullptr;
+        }
+        register size_t i, j;
+        for(i = 0; i < N; ++i) {
+            for(j = 0; j < M; ++j) {
+                // for operators between trivial matrices and neural matrices, the
+                difference -> matrix[i][j] = matrix[i][j] - other -> matrix[i][j] -> data;
+            }
+        }
+        return difference;
+    } else {
+        throw std::length_error("Dimension mismatch");
+    }
+}
+
+#pragma clang diagnostic pop
