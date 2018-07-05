1e2f604eb2d5628bf1cf747f2ce9bca80c2abc15
Oleg Silkin <o.silkin98@gmail.com>
2018-07-05 13:55:15 -0500

    Resolved error in namespacing
    Signed-off-by: Oleg Silkin <o.silkin98@gmail.com>

--- src/matrix_operators.h
+++ src/matrix_operators.h
@@ -19,27 +19,29 @@ namespace neural_networks::utilities::matrix_operators {
     // this is for the user to specify what mode they want to copy the NeuralMatrix object with
     enum Mode { data = 0, function = 1, derivative = 2};
 
-    Matrix<long double> *subtract(const Matrix<long double> *first, const NeuralMatrix *second);
+    components::Matrix<long double> *subtract(const components::Matrix<long double> *first, const NeuralMatrix *second);
 
-    Matrix<long double> *subtract(const NeuralMatrix *first, const Matrix<long double> *second);
+    components::Matrix<long double> *subtract(
+            const NeuralMatrix *first,
+            const components::Matrix<long double> *second);
 
-    Matrix<long double> *hadamard_product(const Matrix<long double> *first, const NeuralMatrix *other);
+    components::Matrix<long double> *hadamard_product(const components::Matrix<long double> *first, const NeuralMatrix *other);
 
-    Matrix<long double> *hadamard_product(const NeuralMatrix *first, const Matrix<long double> *other);
+    components::Matrix<long double> *hadamard_product(const NeuralMatrix *first, const components::Matrix<long double> *other);
 
-    Matrix<long double> *multiply(const Matrix<long double> *first, const NeuralMatrix *other);
+    components::Matrix<long double> *multiply(const components::Matrix<long double> *first, const NeuralMatrix *other);
 
-    Matrix<long double> *multiply(const NeuralMatrix *first, const Matrix<long double> *other);
+    components::Matrix<long double> *multiply(const NeuralMatrix *first, const components::Matrix<long double> *other);
 
-    Matrix<long double> *add(const Matrix<long double> *first, const NeuralMatrix *other);
+    components::Matrix<long double> *add(const components::Matrix<long double> *first, const NeuralMatrix *other);
 
-    Matrix<long double> *add(const NeuralMatrix *first, const Matrix<long double> *other);
+    components::Matrix<long double> *add(const NeuralMatrix *first, const components::Matrix<long double> *other);
 
     /* the field parameter specifies which field to grab from the Neuron objects */
-    Matrix<long double> *create(const NeuralMatrix* other, Mode mod = data);
+    components::Matrix<long double> *create(const NeuralMatrix* other, Mode mod = data);
 
     // to create randomized matrices
-    Matrix<long double> *create_randomized_matrix(size_t N, size_t M = 1,
+    components::Matrix<long double> *create_randomized_matrix(size_t N, size_t M = 1,
             long double mean = 0.0, long double stddev = 2.0);
 
 };
