f31177b68aef4e731aa5dee5c6b01f8ee7032804
Oleg Silkin <o.silkin98@gmail.com>
2018-07-05 12:11:46 -0500

    Added namespace structure to the project
    Signed-off-by: Oleg Silkin <o.silkin98@gmail.com>

---
+++ src/namespaces.h
@@ -0,0 +1,22 @@
+//
+// Created by Oleg on 7/1/2018.
+//
+
+#ifndef NEURALNETWORKS_NAMESPACES_H
+#define NEURALNETWORKS_NAMESPACES_H
+
+namespace neural_networks {
+    namespace utilities {
+        namespace components {
+            class Matrix;
+        }
+        class Neuron;
+        class NeuralMatrix;
+        class NeuralLayer;
+        namespace matrix_operators { } // to be defined elsewhere
+    }
+
+    class NeuralNetwork;
+}
+
+#endif //NEURALNETWORKS_NAMESPACES_H
